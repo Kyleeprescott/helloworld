f6054f463b184b989ec917bf57d905a4b623f9de
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-09-04 13:22:52 -0600

    first commit

---
+++ src/kylee/prescott/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+hello world program print hello world in console 
+ */
+package kylee.prescott.helloworld;
+
+/**
+ * @author kylee.prescott
+ *9/18
+ *hello world 
+ */
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+		System.out.println("hello World");
+	}
+
+}
