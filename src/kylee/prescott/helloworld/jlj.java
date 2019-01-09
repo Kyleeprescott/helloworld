638f12cfd19e2803f2819d946ecd47e8456ed5bd
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2019-01-09 13:08:52 -0700

    work

---
+++ src/kylee/prescott/helloworld/jlj.java
@@ -0,0 +1,23 @@
+package kylee.prescott.helloworld;
+
+public class jlj {
+
+	public static void main(String[] args) {
+	
+
+		 int x = 3;
+int y = 0;
+
+		switch (x + 3) {
+
+		    case 6:  y = 0;
+
+		    case 7:  y = 1;
+
+		    default: y += 1;
+	        
+
+    }
+
+	}
+}
